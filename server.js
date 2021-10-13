4cd12548cd5a231b3097a07364206423b468b0f7
Mjanuary <janviermuhawenimana@gmail.com>
2021-10-13 12:17:23 +0200

    making the server ready for poduction

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 
-const app = express();
+// Configuration
+const SERVER_URL = "http://157.245.142.149";
+const SERVER_PORT = 5000;
 
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(cors());
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://165.22.77.142",
+    origin: SERVER_URL,
     methods: ["GET", "POST"],
   },
 });
@@ -58,7 +60,7 @@ let Events = {
 };
 
 app.get("/liveserver", (req, res) => {
-  console.log("visited");
+  // // console.log("visited");
   res.send({ msg: "Welcome to the live server" });
 });
 
@@ -81,12 +83,12 @@ let ROOM_DETAILS = [
 ];
 
 io.on("connection", (socket) => {
-  console.log("connecting.."); //TODO: to be removed
-  console.log("active: ", CANDIDATES.length); //TODO: to be removed
+  // console.log("connecting.."); //TODO: to be removed
+  // console.log("active: ", CANDIDATES.length); //TODO: to be removed
 
   //******** GET UPDATED ROOM DETAILS ********/
   socket.on(Events.UPDATED_ROOM_LIST, (room_id, SendRooms) => {
-    console.log("Sending updated list..."); //TODO: to be removed
+    // console.log("Sending updated list..."); //TODO: to be removed
     if (room_id !== undefined && room_id !== null) {
       // check if the room exists
       let exists_room = ROOM_DETAILS.find(
@@ -118,7 +120,7 @@ io.on("connection", (socket) => {
 
   //**** user Joined the room */
   socket.on("join-room", (data, getTheRoom) => {
-    console.log(" ---------- connected: " + data.user_id + " ---> ");
+    // console.log(" ---------- connected: " + data.user_id + " ---> ");
 
     //* add users to the database
     CANDIDATES.push(data);
@@ -141,7 +143,7 @@ io.on("connection", (socket) => {
         start_exam: false,
         start_time: null,
       });
-      console.log("ROOM CREATED: ", data.room_id); //TODO: to be removed
+      // console.log("ROOM CREATED: ", data.room_id); //TODO: to be removed
     } else {
       // Let others users know
       socket.to(data.room_id).emit("user-connected", data);
@@ -160,7 +162,7 @@ io.on("connection", (socket) => {
 
     //******** START EXAM ********/
     socket.on(Events.START_EXAM_SERVER, (event) => {
-      console.log("START EXAM: ", event.room_id); //TODO: to be removed
+      // console.log("START EXAM: ", event.room_id); //TODO: to be removed
       if (event.room_id) {
         socket.to(event.room_id).emit(Events.START_EXAM_CLIENT, event);
 
@@ -180,7 +182,7 @@ io.on("connection", (socket) => {
     //******** STOP EXAM ********/
     socket.on(Events.STOP_EXAM_SERVER, (event) => {
       if (event.room_id) {
-        console.log("==== STOP EXAM ===="); //TODO: to be removed
+        // console.log("==== STOP EXAM ===="); //TODO: to be removed
         socket.to(event.room_id).emit(Events.STOP_EXAM_CLIENT, event);
 
         ROOM_DETAILS = ROOM_DETAILS.map((itm) =>
@@ -202,7 +204,7 @@ io.on("connection", (socket) => {
       // remove user to the room
       CANDIDATES = CANDIDATES.filter((itm) => itm.user_id !== data.user_id);
 
-      console.log("disconnected: " + data.user_id); //TODO: to be removed
+      // console.log("disconnected: " + data.user_id); //TODO: to be removed
       console.table(CANDIDATES); //TODO: to be removed
     });
 
@@ -236,7 +238,7 @@ io.on("connection", (socket) => {
     socket.on(Events.CAND_REMOVE_EXAM_SERVER, (event) => {
       if (event.room_id) {
         socket.to(event.room_id).emit(Events.CAND_REMOVE_EXAM_CLIENT, event);
-        console.log("CANDIDATE REMOVED INTO EXAM");
+        // console.log("CANDIDATE REMOVED INTO EXAM");
       }
     });
 
@@ -249,5 +251,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000);
+// server.listen(SERVER_PORT);
+
+server.listen(SERVER_PORT, () => {
+  // // console.log(`App listening at ${SERVER_URL}:${SERVER_PORT}`);
+});
 // peerjs --port 3001
